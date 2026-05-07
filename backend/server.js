6ba644c65991f66db87b611c51ced0a285129388
Bharath <bharathbandi13925@gmail.com>
2026-05-07 10:40:45 +0530

    chnage srever.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
-app.use(express.json());
-
+app.use(
+  cors({
+    origin: "https://street-view-videos.vercel.app",
+    credentials: true,
+  })
+);
 // Enable CORS
 app.use(cors());
 
